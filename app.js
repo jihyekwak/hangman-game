6a1722e61da5194174e371d528d814ae8854bd7a
jihyekwak <jihyekwak53@gmail.com>
2023-02-18 21:13:20 -0800

    Add function for generate random quiz

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+const quizzes = ["ant", "banana", "cat", "dog", "elephant", "fish", "gorilla", "house", "island"];
+
 const alphabetBtns = Array.from(document.querySelectorAll(".alphabet-btn"));
+const startBtn = document.querySelector("#start-btn");
+const quizArea = document.querySelector("#quiz-area");
+
+let randomQuiz = null;
 
 console.log(alphabetBtns);
 
@@ -9,4 +15,25 @@ function onAlphabetBtnClick(event) {
     console.log(chosenAlphabet);
 }
 
+function gameGenerator() {
+    randomQuiz = quizzes[Math.floor(Math.random() * quizzes.length)];
+    console.log(randomQuiz);
+    return randomQuiz;
+}
+
+function onStartBtnClick() {
+    gameGenerator();
+    for( let i = 0 ; i < randomQuiz.length ; i++ ) {
+        const letterBox = document.createElement("div");
+        letterBox.id = randomQuiz[i];
+        letterBox.classList.add("letter-box")
+        quizArea.appendChild(letterBox);
+        const letterSpan = document.createElement("span");
+        letterSpan.classList.add("hidden");
+        letterSpan.innerHTML = randomQuiz[i];
+        letterBox.appendChild(letterSpan);
+    }
+}
+
 alphabetBtns.forEach(alphabetBtn => alphabetBtn.addEventListener("click", onAlphabetBtnClick));
+startBtn.addEventListener("click", onStartBtnClick);
